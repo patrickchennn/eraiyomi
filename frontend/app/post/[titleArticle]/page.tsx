32d9e1caa9cc0ec328316e3ee84a20af4f811925
patrickchennn <patrick.chen31@gmail.com>
2024-02-15 17:15:56 +0700

    add code block highlighter

--- frontend/app/post/[titleArticle]/page.tsx
+++ frontend/app/post/[titleArticle]/page.tsx
@@ -1,4 +1,4 @@
-import { QuillDeltaToHtmlConverter } from "quill-delta-to-html";
+import { BlockGroup, InlineGroup, QuillDeltaToHtmlConverter } from "quill-delta-to-html";
 
 import HeaderSection from "@/components/blog/HeaderSection";
 import LikeDislikeArticle from "@/components/blog/LikeDislikeArticle";
@@ -9,6 +9,7 @@ import convertDate from "@/utils/convertDate";
 
 import hljs from 'highlight.js'
 import 'highlight.js/styles/atom-one-dark.css';
+
 import chalk from "chalk";
 import { Metadata, ResolvingMetadata } from "next";
 import CreateTitle from "@/components/blog/CreateTitle";
@@ -16,8 +17,16 @@ import { GET_articleAsset } from "@/services/article-asset/GET_articleAsset";
 import { getArticle } from "@/services/article/getArticle";
 import DisqusEmbed from "@/components/blog/DisqusEmbed";
 import getReadEstimation from "@/utils/getReadEstimation";
+import dynamic from "next/dynamic";
 
-
+// solve the problem `ReferenceError: window is not defined on client component`
+const DynamicDisqusEmbed = dynamic(
+  () => import('@/components/blog/DisqusEmbed'),
+  { 
+    ssr: false ,
+    loading:()=><div className='loader'></div>
+  }
+)
  
 
 
@@ -88,12 +97,13 @@ export default async function Page({
     )
   }
 
-  let imgIdxs: number[] =[]
   const articleAsset = articleAssetRes.data
   const article = articleRes.data
   
-
-
+  
+  
+  let imgIdxs: number[] =[]
+  // FOR: search all imgs and keep the index
   for(let i=0; i<articleAsset.content.length; i++){
     const data = articleAsset.content[i] as {[key: string]: any}
     if(Object.hasOwn(data.insert,"image")){
@@ -102,39 +112,21 @@ export default async function Page({
   }
 
   // documentation: https://github.com/nozer/quill-delta-to-html#readme
-  const converter = new QuillDeltaToHtmlConverter(
-    articleAsset.content,
-    // TODO: make the code block highlighted
-    // {
-    //   customTag(format, op) {
-    //     // console.log(format)
-    //     // console.log(op)
-    //     if(op.isCodeBlock()){
-    //       // console.log("op.isCodeBlock()")
-    //     }
-    //   },
-    //   customTagAttributes(op) {
-    //     if(op.isCodeBlock()){
-    //       console.log("op.isCodeBlock()")
-    //       return {class:"language-javascript"}
-    //     }
-    //   },
-    // }
-  );
+  const converter = new QuillDeltaToHtmlConverter(articleAsset.content);
 
   // this function is supposed to return a `string` type. But I had no idea what to return, I did not understand the doc
   // currently, it returns void, obviously, an error type. So to simplify task, I just add `@ts-ignore`
   // @ts-ignore
   converter.beforeRender((groupType, data)=>{
-    // console.log("groupType=",groupType)
-    // console.log("data=\t",data.ops)
-    // console.log()
     if(groupType==="inline-group"){
-      // @ts-ignore
-      for(const op of data.ops){
+      // console.log(chalk.magenta.bgBlack("IF: groupType===inline-group"))
+      const inlineData = data as InlineGroup
+      // console.log("inlineData=",inlineData)
+
+      for(const op of inlineData.ops){
         
         /* fail implementation of feature
-          the main problem: https://github.com/nozer/quill-delta-to-html/issues/95
+          the main problem: "img src attribute is set to [object Object] #95": https://github.com/nozer/quill-delta-to-html/issues/95
           
           This supposed to help me constructing an `img` tag that is looks like: <img src="" alt="" data-[attribute]/>. You see there are several attributes on that img tag, and I want those attributes to, of course, enrich the `img` information. Current solution I can only use, `op.insert.value = src`, to set the `src` attribute which you can see in here
 
@@ -147,22 +139,59 @@ export default async function Page({
           so, in short, i can only set the `src` attribute, I can't add like attribute `alt`, let alone the `data-` attribute
         */
         if(op.insert.type==="image"){
-          // console.log("found an image")
-          // console.log(op)
-          const imgObj = articleAsset.content[imgIdxs[0]] as {[key: string]: any}
-          const src = imgObj.insert.image.src
-          // const dataFilename = articleAsset.content.quill[imgIdxs[0]].insert.image["data-filename"]
+          // console.log(chalk.magenta.bgBlack("found an image"))
+          // console.log("op=",op)
+          // console.log("op.insert=",op.insert)
+          // console.log("op.insert.value=",op.insert.value)
+
           // console.log("idx=",imgIdxs[0])
-          // console.log("dataFilename=",dataFilename)
-          // console.log("src=",src.slice(0,30))
+
+          const imgObj = articleAsset.content[imgIdxs[0]] as {[key: string]: any}
+          // console.log("imgObj=",imgObj)
+
+          const src = imgObj.insert.image.src
+
+          // const dataPublicId = imgObj.insert.image["data-public_id"]
+
+          
           // pop the first element for preparing the next img
           imgIdxs.shift()
+
+          // set img src
+          // original error: Cannot assign to 'value' because it is a read-only property.ts(2540)
+          // @ts-ignore
           op.insert.value = src
+
+          // set additional attributes for the img
           // op.attributes = {
-          //   "data-file-name":dataFilename
+          //   "data-file-name":dataPublicId
           // }
         }
+      }
+    }else if(groupType==="block"){
+      // console.log(chalk.magenta.bgBlack("IF: groupType===block"))
+      // `blockData` points to `data`. This existed for sake of typecasting. Initially I can use the `as` typescript typecast utility but it took many variable to typecast for each
+      const blockData = data as BlockGroup
 
+      // console.log("data=",data)
+      // console.log("data.op=",data.op)
+      
+      // make the code block highlighted
+      if(blockData.op.isCodeBlock()){
+        // console.log(chalk.magenta.bgBlack("IF: op.isCodeBlock()"))
+        // console.log("data=",data)
+        // console.log("data.op=",data.op)
+        // console.log("data.ops=",data.ops)
+
+        let codeContent = blockData.ops.map(op => op.insert.value).join('');
+        // console.log("codeContent=",codeContent)
+
+        // const highlightedCode = hljs.highlight(codeContent, { language: 'python' })
+        // console.log("highlightedCode=",highlightedCode)
+
+        const autoHighlightCode = hljs.highlightAuto(codeContent)
+
+        return `<pre class="hljs"><code>${autoHighlightCode.value}</code></pre>`;
       }
     }
   });
@@ -213,32 +242,8 @@ export default async function Page({
         {/* <TableOfContents /> */}
 
         {/* Disqus library, client component */}
-        <DisqusEmbed articleId={article._id}/>
+        <DynamicDisqusEmbed articleId={article._id}/>
       </div>
     </>
   )
 }
-
-// function Commentary(){
-//   /**
-//   *  RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
-//   *  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables    */
-//   /*
-//   */
-//   var disqus_config = function () {
-//     this.page.url = metadata.title;  // Replace PAGE_URL with your page's canonical URL variable
-//     // this.page.identifier = PAGE_IDENTIFIER; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
-//   };
-
-//   (function() { // DON'T EDIT BELOW THIS LINE
-//     var d = document;
-//     const s = d.createElement('script');
-//     s.src = 'https://eraiyomi.disqus.com/embed.js';
-//     s.setAttribute('data-timestamp', +new Date());
-//     (d.head || d.body).appendChild(s);
-//   })();
-//   return (
-//     <div id="disqus_thread"></div>
-
-//   )
-// }
