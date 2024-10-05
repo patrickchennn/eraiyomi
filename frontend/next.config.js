cdb2666e84f05f5dbe4fbabf40e0b676eec5d91d
patrickchennn <patrick.chen31@gmail.com>
2024-10-05 23:55:33 +0700

    v0.1.0

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,34 +1,20 @@
-const { PHASE_DEVELOPMENT_SERVER, PHASE_TEST} = require('next/constants')
-
-module.exports = (phase, { defaultConfig }) => {
-  /**
-   * @type {import('next').NextConfig}
-   */
-  if (phase === PHASE_DEVELOPMENT_SERVER) {
-    return {
-      /* development only config options here */
-      env:{
-        URL_API:"http://localhost:8000/api",
-      },
-      images: {
-        domains: ['source.unsplash.com','lh3.googleusercontent.com'],
-      },
-      experimental: {
-        serverActions: true,
-      },
-    }
-  }
-  
-  return {
-    env:{
-      URL_API:"https://eraiyomi-server.up.railway.app/api"
-    },
-    images: {
-      domains: ['source.unsplash.com','lh3.googleusercontent.com'],
-    },
-    experimental: {
-      serverActions: true,
-    },
-    /* config options for all phases except development here */
-  }
+// @ts-check
+ 
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+  /* config options here */
+  env:{
+    URL_API:"http://localhost:8000/api",
+  },
+  images: {
+    domains: ['source.unsplash.com','lh3.googleusercontent.com'],
+  },
+  experimental: {
+    serverActions: true,
+  },
+  output: "standalone"
 }
+ 
+module.exports = nextConfig
